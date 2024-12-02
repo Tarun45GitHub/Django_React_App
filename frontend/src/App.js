3dff5837cd49d75b5a0688d0447c2e6b9c62867f
TARUN DALBERA <dalberatarun1@gmail.con>
2024-12-02 19:38:04 +0530

    router add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-
 // import './App.css';
+import React from "react";
+import {Route,Routes} from 'react-router-dom'
+import Home  from "./components/Home";
+import About from "./components/About";
+import Create from "./components/Create";
 
 function App() {
   return (
-    <div>
-      This is our react page
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/create" element={<Create/>}/>
+      </Routes>
     </div>
   );
 }
